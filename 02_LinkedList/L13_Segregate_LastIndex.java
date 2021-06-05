7784fcea81fb269f20377cafe926be7c3621604b
rishabhsinghal1007 <rishabhsinghal1007@gmail.com>
2021-06-05 23:44:31 +0530

    commit

---
+++ 02_LinkedList/L13_Segregate_LastIndex.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class L13_Segregate_LastIndex {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode segregateOnLastIndex(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode small = new ListNode(-1);
+        ListNode large = new ListNode(-1);
+        ListNode sp = small, lp = large, curr = head;
+
+        ListNode pivotNode = head;
+        while (pivotNode.next != null)
+            pivotNode = pivotNode.next;
+
+        while (curr != null) {
+            if (curr.val <= pivotNode.val) {
+                sp.next = curr;
+                sp = sp.next;
+            } else {
+                lp.next = curr;
+                lp = lp.next;
+            }
+            curr = curr.next;
+        }
+
+        sp.next = large.next;
+        lp.next = null;
+
+        return sp;
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+
+    public static ListNode createList(int n) {
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        int n = scn.nextInt();
+        ListNode h1 = createList(n);
+        h1 = segregateOnLastIndex(h1);
+        printList(h1);
+    }
+}
