0a1e1c9ad51f764c9371decaf41f8b04cd331273
rishabhsinghal1007 <rishabhsinghal1007@gmail.com>
2021-07-21 23:36:09 +0530

    commit

--- 04_Graph/l001.java
+++ 04_Graph/l001.java
@@ -104,6 +104,133 @@ public class l001 {
         System.out.println(components);
     }
 
+    public static void BFS_WithCycle(ArrayList<Edge>[] graph, int src, boolean[] vis) {
+        int level = 0, cycleCount = 0;
+        boolean iscycle = false;
+
+        LinkedList<Integer> que = new LinkedList<>();
+        que.add(src);
+
+        while (que.size() != 0) {
+            int size = que.size();
+            System.out.print(level + " -> ");
+
+            while (size-- > 0) {
+                int rvtx = que.removeFirst();
+
+                if (vis[rvtx]) {
+                    cycleCount++;
+                    iscycle = true;
+                    continue;
+                }
+
+                System.out.print(rvtx + " ");
+                vis[rvtx] = true;
+                for (Edge e : graph[rvtx]) {
+                    if (!vis[e.v])
+                        que.addLast(e.v);
+                }
+            }
+            System.out.println();
+            level++;
+        }
+        System.out.println("CycleCount : " + cycleCount);
+        System.out.println("Is Cycle Present : " + iscycle);
+    }
+
+    public static void BFS_WithoutCycle(ArrayList<Edge>[] graph, int src, boolean[] vis) {
+        int level = 0;
+
+        LinkedList<Integer> que = new LinkedList<>();
+        que.addLast(src);
+
+        vis[src] = true;
+        while (que.size() != 0) {
+            int size = que.size();
+            System.out.print(level + ": ");
+
+            while (size-- > 0) {
+                int rvtx = que.removeFirst();
+
+                System.out.print(rvtx + " ");
+                for (Edge e : graph[rvtx]) {
+                    if (!vis[e.v]) {
+                        que.addLast(e.v);
+                        vis[e.v] = true;
+                    }
+                }
+            }
+
+            System.out.println();
+            level++;
+        }
+    }
+
+    public static boolean is_Bipartite(ArrayList<Edge>[] graph, int src, int[] vis) {
+        LinkedList<Integer> que = new LinkedList<>();
+        que.addLast(src);
+
+        int color = 0; // 0 : red , 1 : green
+        boolean isBipartite = true;
+        boolean isCycle = false;
+
+        while (que.size() != 0) {
+            int size = que.size();
+
+            while (size-- > 0) {
+                int rvtx = que.removeFirst();
+
+                if (vis[rvtx] != -1) {
+                    isCycle = true;
+                    if (vis[rvtx] != color) {
+                        isBipartite = false;
+                        break;
+                    }
+                    continue;
+                }
+
+                vis[rvtx] = color;
+
+                for (Edge e : graph[rvtx]) {
+                    if (vis[e.v] == -1) {
+                        que.addLast(e.v);
+                    }
+                }
+
+            }
+            color = (color + 1) % 2;
+
+            if (!isBipartite)
+                break;
+        }
+        if (isCycle) {
+            if (isBipartite)
+                System.out.println("Graph is Bi-Partite it means it has even length cycle");
+            else
+                System.out.println("Graph is Non Bi-Partite it means it has odd length cycle");
+
+        } else {
+            System.out.println("Graph is Bi-Partite");
+        }
+
+        return isBipartite;
+    }
+
+    public static boolean isBipartite(ArrayList<Edge>[] graph) {
+        int N = graph.length;
+        int[] vis = new int[N];
+        Arrays.fill(vis, -1);
+
+        boolean res = true;
+        for (int i = 0; i < N; i++) {
+            if (vis[i] == -1) {
+                res = res && is_Bipartite(graph, i, vis);
+            }
+        }
+        return res;
+
+    }
+
     public static void constructGraph() {
         int N = 7;
         ArrayList<Edge>[] graph = new ArrayList[N];
@@ -118,11 +245,18 @@ public class l001 {
         addEdge(graph, 4, 5, 2);
         addEdge(graph, 5, 6, 3);
         addEdge(graph, 6, 4, 8);
+        // addEdge(graph, 2, 7, 10);
+        // addEdge(graph, 2, 8, 10);
+        // addEdge(graph, 7, 8, 10);
 
-        display(graph);
+        // display(graph);
         boolean[] vis = new boolean[N];
         // System.out.println(dfs_findPath(graph, 0, 6, vis));
-        System.out.println(printAllPath(graph, 0, 6, "", 0, vis));
+        // System.out.println(printAllPath(graph, 0, 6, "", 0, vis));
+        // getConnectedComponent(graph);
+        // BFS_WithCycle(graph, 0, vis);
+        // BFS_WithoutCycle(graph, 0, vis);
+        System.out.println(isBipartite(graph));
     }
 
     public static void main(String[] args) {
