c10a6fbdc8e98a6b9e85aabfbd0b6ba86d0937ee
rishabhsinghal1007 <rishabhsinghal1007@gmail.com>
2021-08-09 22:58:31 +0530

    commit

---
+++ 05_DP/gold_Mine.java
@@ -0,0 +1,63 @@
+public class gold_Mine {
+    
+    public static int goldMine_memo_(int sr, int sc, int[][] mat, int[][] dir, int[][] dp) {
+        if (sc == mat[0].length - 1) {
+            return dp[sr][sc] = mat[sr][sc];
+        }
+
+        if (dp[sr][sc] != -1)
+            return dp[sr][sc];
+
+        int maxGold = 0;
+        for (int[] d : dir) {
+            int r = sr + d[0];
+            int c = sc + d[1];
+
+            if (r >= 0 && c >= 0 && r < mat.length && c < mat[0].length)
+                maxGold = Math.max(maxGold, goldMine_memo_(r, c, mat, dir, dp) + mat[sr][sc]);
+        }
+
+        return dp[sr][sc] = maxGold;
+    }
+
+    public static int goldMine_memo(int n, int m, int[][] mat) {
+        int[][] dir = { { -1, 1 }, { 1, 1 }, { 0, 1 } };
+        int[][] dp = new int[n][m];
+
+        int maxGold = 0;
+        for (int r = 0; r < n; r++) {
+            maxGold = Math.max(maxGold, goldMine_memo_(r, 0, mat, dir, dp));
+        }
+
+        return maxGold;
+    }
+
+    public static int goldMine_Tabu(int SR, int SC, int[][] mat, int[][] dir, int[][] dp) {
+        for (int sc = mat[0].length - 1; sc >= SC; sc--) {
+            for (int sr = mat.length - 1; sr >= SR; sr--) {
+                if (sc == mat[0].length - 1) {
+                    dp[sr][sc] = mat[sr][sc];
+                    continue;
+                }
+
+                int maxGold = 0;
+                for (int[] d : dir) {
+                    int r = sr + d[0];
+                    int c = sc + d[1];
+
+                    if (r >= 0 && c >= 0 && r < mat.length && c < mat[0].length)
+                        maxGold = Math.max(maxGold, dp[r][c] + mat[sr][sc]);
+                }
+
+                dp[sr][sc] = maxGold;
+            }
+        }
+
+        int maxGold = 0;
+        for (int r = 0; r < mat.length; r++) {
+            maxGold = Math.max(maxGold, dp[r][0]);
+        }
+
+        return maxGold;
+    }
+}
